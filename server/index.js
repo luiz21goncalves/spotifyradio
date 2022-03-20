9579e797bd6a7ea45d2503822b04cf1b55905ce7
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-03-20 11:02:58 -0300

    chore: config deploy

--- server/index.js
+++ server/index.js
@@ -5,3 +5,6 @@ import { logger } from "./utils.js";
 server
   .listen(config.port)
   .on('listening', () => logger.info(`server running at ${config.port}!`))
+
+process.on('uncaughtException', (error) => logger.info(`uncaughtException happened: ${error.stack || error}`))
+process.on('unhandledRejection', (error) => logger.info(`unhandledRejection happened: ${error.stack || error}`))
